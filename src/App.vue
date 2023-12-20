56d5d80418867f2dc3d1fb8376d4485dcbe42ee5
LucaCaligola <luca.caligola@gmail.com>
2023-12-20 15:26:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import AppSearch from './components/AppSearch.vue';
+import AppMain from './components/AppMain.vue';
+
+components: {
+        AppSearch,
+        AppMain
+    }
+    
+</script>
+
+<template>
+  
+
+
+</template>
+
+<style scoped>
+
+</style>
