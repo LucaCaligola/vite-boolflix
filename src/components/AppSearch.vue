cb95e2f7b06737b8df3de84dc02b9f39d16f89ee
LucaCaligola <luca.caligola@gmail.com>
2023-12-20 17:16:41 +0100

    axios now is working properly

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,57 +1,53 @@
 <template>
     <header>
-        <div class="container d-flex justify-content-between align-items-center">
-            <span>BOOLFLIX</span>
-            <input type="text" placeholder="Cerca un titolo">
-        </div>
+      <div class="container d-flex justify-content-between align-items-center">
+        <span>BOOLFLIX</span>
+        <input type="text" placeholder="Cerca un titolo" />
+      </div>
     </header>
-    
-</template>
-<script>
-
-import axios from 'axios';
-
-
-export default {
-
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
     data() {
-        return {
-            filmsList : [],
-        }
+      return {
+        filmsList: [],
+        apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=0a1a18272fb8d93351b833485b06018e&query=ritorno+al+futuro',
+      };
     },
     methods: {
-        getFilms(){
-
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=0a1a18272fb8d93351b833485b06018e&query=ritorno+al+futuro')
-                .then(function (response) {
-                    // handle success
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-                .finally(function () {
-                    // always executed
-                });
-
-        },
-        created() {
-            this.getFilms();
-        },
-        
-    }
-}
-</script>
-<style lang="scss">
-
-.container {
+      getFilms() {
+        axios.get(this.apiUrl)
+          .then((response) => {
+            // handle success
+            console.log(response.data.results);
+          })
+          .catch((error) => {
+            // handle error
+            console.error(error);
+          })
+          .finally(() => {
+            // always executed
+          });
+      },
+    },
+    created() {
+      this.getFilms();
+    },
+  };
+  </script>
+  
+  <style lang="scss">
+  .container {
     height: 80px;
-    width: 100%; 
-}
-    
-header {
+    width: 100%;
+  }
+  
+  header {
     background-color: black;
-    color: red;  
-}
-</style>
+    color: red;
+  }
+  </style>
+  
